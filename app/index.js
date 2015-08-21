fca47129b416665888e639abb03c710be98428a5
Brecht Billiet <brecht.billiet@persgroep.be>
2015-08-21 07:27:53 +0200

    fixed racing condition in bootstrapping of app

--- app/index.js
+++ app/index.js
@@ -1,12 +1,9 @@
 (function() {
 	'use strict';
-	angular.module('app', []).run(function() {
-		console.log('bootstrapped');
-	});
-	angular.bootstrap(document, ['app', 'app.about', 'app.core',
-		'app.customer', 'app.home',
-		'app.login', 'app.project'
-	], {
-		strictDi: true
+	angular.module('app', ['app.about', 'app.core', 'app.customer', 'app.home', 'app.login', 'app.project']);
+	angular.element(document).ready(function() {
+		angular.bootstrap(document, ['app'], {
+			strictDi: true
+		});
 	});
 }());
